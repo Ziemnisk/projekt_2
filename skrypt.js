7534549c280cbf62e04f54df73650df863b7190e
Szymon <pagminator@gmail.com>
2026-03-31 14:17:01 +0200

    dodanie wyzwan

---
+++ skrypt.js
@@ -0,0 +1,22 @@
+const wyzwania = [
+    "Zrób 10 pompek",
+    "Idź na 30 minutowy spacer",
+    "Podnieś 20 kilo na sztandze",
+    "Przeczytaj 20 stron książki",
+    "Wypij 2 litry wody",
+    "Zrób porządek na biurku",
+    "Spędź godzinę bez telefonu",
+    "Zjedz ślimaka",
+    "Przebiegnij 2 km",
+    "Zrób 30 przysiadów",
+    "Posłuchaj polityków muzyki",
+    "Zrób Obraz",
+    "Pomóż bezdomnemu",
+    "Rób deskę przez minutę",
+    "Wyjdź na zakupy za kogoś",
+    "Zorganizuj swoje pliki",
+    "Zrób porządek u kogoś",
+    "Uśmiechnij się do 5 osób",
+    "Zaplanuj jutrzejszy dzień",
+    "Podziękuj za ten dzień"
+];
